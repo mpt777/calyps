ba98b0a0c43b407a99316b7bea80347fda275dd3
Marshal <mptaylor777@gmail.com>
2025-02-09 16:25:19 -0700

    slimming

--- client/src/routes/(app)/projects/widgets/secretsanta/[ssid]/+page.svelte
+++ client/src/routes/(app)/projects/widgets/secretsanta/[ssid]/+page.svelte
@@ -48,7 +48,7 @@
 			<div class="input-group divide-surface-200-800 grid-cols-[auto_1fr_auto] divide-x">
 				<div class="input-group-cell preset-tonal-surface">Your Name</div>
 				<input type="text" class="input--min-w-4" bind:value={inputedName}/>
-				<a class="btn h-full preset-filled-primary-500 rounded-none cursor-pointer" onclick="{() => findGiftee()}">Find!</a>
+				<button type="button" class="btn h-full preset-filled-primary-500 rounded-none cursor-pointer" onclick="{() => findGiftee()}">Find!</button>
 			</div>
 
 			<div class="flex justify-center">
