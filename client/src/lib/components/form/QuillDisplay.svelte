bbe12c1a25b41cf5ac1f1a266a56588331c3a4ca
Marshal <mptaylor777@gmail.com>
2025-02-09 17:31:28 -0700

    slimming

--- client/src/lib/components/form/QuillDisplay.svelte
+++ client/src/lib/components/form/QuillDisplay.svelte
@@ -4,4 +4,6 @@
   // let safeContent = DOMPurify.sanitize(content);
 </script>
 
-<div>{@html content}</div>
+<div class=" ql-container ql-snow !border-none">
+  <div class="ql-editor !p-0 ">{@html content}</div>
+</div>
