b2336f2e0d037ab855c696fec7343b75dcced054
Marshal <mptaylor777@gmail.com>
2025-02-14 18:10:26 -0700

    collections

--- client/src/routes/(app)/recipe/+page.server.js
+++ client/src/routes/(app)/recipe/+page.server.js
@@ -1,7 +1,13 @@
 import { papi } from "$utils/api";
 
-export async function load({ params, fetch }) {
-    const recipes = await(await papi(fetch, `recipe/search/recipe/`, {method: "GET"})).json();
+export async function load({ params, fetch, url }) {
+    let searchQuery = ""
+    let search = url.searchParams.get('search');
+    if (search) {
+        searchQuery = `?search=${search}`
+    }
+
+    const recipes = await(await papi(fetch, `recipe/search/recipe/${searchQuery}`, {method: "GET"})).json();
 
     return { recipes };
 }
--- client/src/routes/(app)/recipe/+page.svelte
+++ client/src/routes/(app)/recipe/+page.svelte
@@ -4,7 +4,7 @@
     import { url } from "$utils/url.js";
     import path from "path";
 
-  let {data} = $props();
+    let {data} = $props();
 
 </script>
 
@@ -13,14 +13,17 @@
 </svelte:head>
 
 <div class="flex items-center gap-4 justify-center">
-  <div class="input-group divide-surface-200-800 grid-cols-[auto_1fr_auto] divide-x items-center">
-    <button type="button">
-      <i class="ri-search-line p-4"></i>
-    </button>
+  <form method="GET">
+    <div class="input-group divide-surface-200-800 grid-cols-[auto_1fr_auto] divide-x items-center">
+      
+      <button type="submit" aria-label="Search">
+        <i class="ri-search-line p-4"></i>
+      </button>
 
-    <input type="text" name="search" placeholder="Search">
+      <input type="text" name="search" placeholder="Search">
 
-  </div>
+    </div>
+  </form>
   {#if page.data.user}
   <div class="text-center">
     <a class="preset-filled-tertiary-500 btn btn--scale" href="{url('recipe_add')}">Add Recipe <i class="ri-sticky-note-add-fill"></i></a>
